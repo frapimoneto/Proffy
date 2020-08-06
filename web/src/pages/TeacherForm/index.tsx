096c432a9ec8a334d3b1adaff2bbef0e2e3a24f3
Francisco Pinto Moreira Neto <31639140+frapimoneto@users.noreply.github.com>
2020-08-05 23:26:28 -0300

    landing page

---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './styles.css';
+import PageHeader from '../../components/PageHeader';
+
+function TeacherForm() {
+  return (
+    <div id="page-teacher-form" className="container">
+      <PageHeader title="Que incível que você quer dar aulas."/>
+    </div>
+  )
+}
+
+export default TeacherForm;
